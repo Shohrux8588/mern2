a485b7e14f6ae7fff87c05abdecfba8911a13c7b
Shohrux8588 <70914015+Shohrux8588@users.noreply.github.com>
2021-11-08 23:31:24 +0500

    Update index.js

--- back/index.js
+++ back/index.js
@@ -14,9 +14,9 @@ app.use(cors());
 app.use("/todos", todoRouter);
 
 // mongodb url
-// mongodb+srv://shakhrukh:test1234@cluster0.jhlct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;
 
-const CONNECTION_URL = "mongodb+srv://shakhrukh:test1234@cluster0.jhlct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const CONNECTION_URL = ""; // mongoDB atlas connect api
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
